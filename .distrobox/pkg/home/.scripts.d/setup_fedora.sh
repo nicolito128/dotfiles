26175f8cf0bc19f5ad6526203ef0dfefd9dd81dc
Nicolas <serna.nicolas.abel@gmail.com>
2025-04-20 19:59:41 -0300

    Create setup_fedora.sh

---
+++ .distrobox/pkg/home/.scripts.d/setup_fedora.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+echo "Installing compilers..."
+
+sudo dnf install -y go gcc g++
+
+echo "Adding VSCode repository..."
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+echo -e \
+ "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" \
+ | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+dnf check-update
+
+echo "Installing VSCode..."
+
+sudo dnf install -y code
+
+echo "Exporting Code application with distrobox"
+
+distrobox-export --app code

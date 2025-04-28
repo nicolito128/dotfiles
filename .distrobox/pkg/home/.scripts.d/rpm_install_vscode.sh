1f2a8e692a81482a25087899a5d07db543523627
Nicolas <serna.nicolas.abel@gmail.com>
2025-04-28 02:00:53 -0300

    Create rpm_install_vscode.sh

---
+++ .distrobox/pkg/home/.scripts.d/rpm_install_vscode.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+echo "Adding Microsoft and VSCode repository..."
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+echo \
+  -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" \
+  | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+echo "Installing VSCode..."
+
+dnf check-update
+sudo dnf install code
+
+echo "Exporting Code application"
+
+distrobox-export --app code

6ce7bd480d1033122140d0c18aed778ae854075c
Nicolas <serna.nicolas.abel@gmail.com>
2025-06-04 15:10:44 -0300

    Add files via upload

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,16 @@
 -- Key bindings and shortcuts
 
+local mset = vim.keymap.set
+
 -- Explorer
-vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory with Oil" })
+mset("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory with Oil" })
 
 -- Copy to clipboard
-vim.keymap.set('v', "<leader>y", '"+y')
+mset('v', "<leader>y", '"+y')
 
 -- Don't replace the content of buffer when pasting
-vim.keymap.set("x", "p", "pgv\" . v:register . 'y'", { expr = true })
+mset("x", "p", "pgv\" . v:register . 'y'", { expr = true })
+
+-- Go JSON tag
+mset('n', '<leader>j', '_ywA<Space><Esc>pbeld$bguwciw"<Esc>pA"<Esc>F"<Ignore>ijson:<Esc>bi`<Esc>A`<Esc>_j', { noremap = true})
+

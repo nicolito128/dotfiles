7a6c7c3920bbfaff7c8d5762a7e594f9f56f848d
Nicolas <serna.nicolas.abel@gmail.com>
2025-06-25 14:34:08 -0300

    Update lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,29 +10,27 @@ return {
             'mason-org/mason.nvim',
             'neovim/nvim-lspconfig',
         },
-        opts = {
-            ensure_installed = {
-                'lua_ls',
-                'gopls',
-                'clangd',
-                'elp', -- erlang
-                'lexical', -- elixir
-                'ts_ls',
-                'pylsp', -- python
-                'phpactor',
-                'marksman', -- markdown
-                'taplo', -- toml
-                'julials',
-                'html',
-                'cssls',
-                'yamlls',
-            },
-        },
         config = function()
             local lsp_capabilities = require('blink.cmp').get_lsp_capabilities()
             local lspconfig = require('lspconfig')
 
             require('mason-lspconfig').setup({
+                ensure_installed = {
+                    'lua_ls',
+                    'gopls',
+                    'clangd',
+                    'elp', -- erlang
+                    'lexical', -- elixir
+                    'ts_ls',
+                    'pylsp', -- python
+                    'phpactor',
+                    'marksman', -- markdown
+                    'taplo', -- toml
+                    'julials',
+                    'html',
+                    'cssls',
+                    'yamlls',
+                },
                 handlers = {
                     function(server)
                         lspconfig[server].setup({

64be7381f0bf7c447e740c52d37c34ad31f3dd9c
Nicolas A. Serna <serna.nicolas.abel@gmail.com>
2026-06-27 19:41:12 -0300

    auto-up: .config/nvim/

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,41 +2,45 @@
 local mset = vim.keymap.set
 
 -- Copy to clipboard
-mset('v', "<leader>y", '"+y')
+mset("v", "<leader>y", '"+y')
 
 -- Fast move with ctrl-up/down
-mset('n', '<C-Down>', '5j', { noremap = true, silent = true })
-mset('n', '<C-Up>', '5k', { noremap = true, silent = true })
+mset("n", "<C-Down>", "5j", { noremap = true, silent = true })
+mset("n", "<C-Up>", "5k", { noremap = true, silent = true })
 
-mset('n', '<C-j>', '5j', { noremap = true, silent = true })
-mset('n', '<C-k>', '5k', { noremap = true, silent = true })
+mset("n", "<C-j>", "5j", { noremap = true, silent = true })
+mset("n", "<C-k>", "5k", { noremap = true, silent = true })
 
-mset('n', '<C-l>', '5l', { noremap = true, silent = true })
-mset('n', '<C-h>', '5h', { noremap = true, silent = true })
+mset("n", "<C-l>", "5l", { noremap = true, silent = true })
+mset("n", "<C-h>", "5h", { noremap = true, silent = true })
 
 -- Don't replace the content of buffer when pasting
 mset("x", "p", "pgv\" . v:register . 'y'", { expr = true })
 
 -- Go JSON tag
-mset('n', '<leader>j', '_ywA<Space><Esc>pbeld$bguwciw"<Esc>pA"<Esc>F"<Ignore>ijson:<Esc>bi`<Esc>A`<Esc>_j',
-  { noremap = true })
+mset(
+	"n",
+	"<leader>j",
+	'_ywA<Space><Esc>pbeld$bguwciw"<Esc>pA"<Esc>F"<Ignore>ijson:<Esc>bi`<Esc>A`<Esc>_j',
+	{ noremap = true }
+)
 
 -- Ignore variable in Go/Zig
-mset('n', '<leader>_', 'ebcw_', { noremap = true, desc = 'Replace word with _ (to ignore variable)' })
+mset("n", "<leader>_", "ebcw_", { noremap = true, desc = "Replace word with _ (to ignore variable)" })
 
 -- Toggle diagnostics
 local diagnostic_enabled = true
 
 local function toggle_diagnostics()
-  diagnostic_enabled = not diagnostic_enabled
-  vim.diagnostic.enable(diagnostic_enabled)
-  print("Diagnostics " .. (diagnostic_enabled and "enabled" or "disabled"))
+	diagnostic_enabled = not diagnostic_enabled
+	vim.diagnostic.enable(diagnostic_enabled)
+	print("Diagnostics " .. (diagnostic_enabled and "enabled" or "disabled"))
 end
 
-mset('n', '<leader>d', toggle_diagnostics, { noremap = true, silent = true })
+mset("n", "<leader>d", toggle_diagnostics, { noremap = true, silent = true })
 
 -- Add ; to the end of the line in normal mode
-mset('n', ';;', 'm`A;<Esc>``', { noremap = true, desc = 'Add ; to the end of the line' })
+mset("n", ";;", "m`A;<Esc>``", { noremap = true, desc = "Add ; to the end of the line" })
 
 -- 's' key to change a word without replacing the content of the buffer
-mset('n', 's', 'cl', { noremap = true, silent = true })
+mset("n", "s", "cl", { noremap = true, silent = true })
--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,28 +1,42 @@
 vim.lsp.enable({
-    'gopls',
-    'lua_ls'
+	"gopls",
+	"lua_ls",
 })
 
 vim.diagnostic.config({
-    virtual_lines = true,
-    -- virtual_text = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-    float = {
-        border = 'rounded',
-        source = true,
-    },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = '󰅚 ',
-            [vim.diagnostic.severity.WARN] = '󰀪 ',
-            [vim.diagnostic.severity.INFO] = '󰋽 ',
-            [vim.diagnostic.severity.HINT] = '󰌶 ',
-        },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
-            [vim.diagnostic.severity.WARN] = 'WarningMsg',
-        },
-    },
+	virtual_lines = true,
+	-- virtual_text = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = true,
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+		},
+	},
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Opciones para los mapeos del buffer actual
+		local opts = { buffer = ev.buf, remap = false, silent = true }
+
+		-- Go to declaration
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+		-- Go to definition
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	end,
 })
